bb96ff71ff1a613b2e6d647e8979aac0f97f3364
scald97 <serena.ann.caldarella@gmail.com>
2022-06-10 22:17:09 -0400

    template error code

--- src/main/java/org/wecancoeit/reviews/ReviewRepository.java
+++ src/main/java/org/wecancoeit/reviews/ReviewRepository.java
@@ -15,10 +15,11 @@ public class ReviewRepository {
 
     public ReviewRepository(){
         Review Food = new Review(1L, "Shuwa", "Lamb meat seasoned well with fresh garlic, salt, nutmeg, cumin, coriander, paprika,black pepper, and tumeric and wrapped in banana leaves buried to cook over the coals in the ground  ", "Food");
-        Review Attraction = new Review(2L,"Wadis","Oman is wealthy with wadis (a low, dry valley. The term wadi is most commonly used in Arabic-speaking parts of the world. This word wadi is mainly used to describe valleys and dry creeks and riverbeds in the Middle East and North Africa. A wadi might be a stream during the rainy season and a dry ravine during the rest of the year.) There are a few that are full of water all year around, Wadi Beni Khalid is the most beautiful place I have gotten to experiance on earth.","Places");
-
+        Review Attraction = new Review(2L,"Wadis","Oman is wealthy with wadis (a low, dry valley. The term wadi is most commonly used in Arabic-speaking parts of the world. This word wadi is mainly used to describe valleys and dry creeks and riverbeds in the Middle East and North Africa. A wadi might be a stream during the rainy season and a dry ravine during the rest of the year.) There are a few that are full of water all year around, Wadi Beni Khalid is the most beautiful place I have gotten to experiance on earth.","Attraction");
+        Review Lodging = new Review(3L,"Camping","Camping in Oman is taking a mat and a sleeping bag to an alcoved beach and sleeping under the stars","Lodging");
         reviewList.put(Food.getId(), Food);
         reviewList.put(Attraction.getId(),Attraction);
+        reviewList.put(Lodging.getId(),Lodging);
     }
 
 
